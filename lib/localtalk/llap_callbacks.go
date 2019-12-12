9e4fd79128f2a9a5e291dd61cebfcec7bf18e8a3
Rob Mitchelmore <cheesey@prydwen.local>
2019-12-11 22:06:38 -0800

    improve callback documentation

--- lib/localtalk/llap_callbacks.go
+++ lib/localtalk/llap_callbacks.go
@@ -4,12 +4,16 @@ import (
 	"sync"
 )
 
+// A CallbackChain value stores an unordered set of callback functions to be
+// called when some kind of packet reception event occurs.
 type CallbackChain struct {
 	l sync.RWMutex
 	
 	m map[*func(l *LLAPPacket)]struct{} // well this is a hack
 }
 
+// Add adds a function to the callback chain.  Callbacks must NOT mutate the
+// packet they are given.
 func (c *CallbackChain) Add(p *func(l *LLAPPacket)) {
 	c.l.Lock()
 	defer c.l.Unlock()
@@ -21,6 +25,10 @@ func (c *CallbackChain) Add(p *func(l *LLAPPacket)) {
 	c.m[p] = struct{}{}
 }
 
+// Remove removes a function from the callback chain.  Note that the function
+// pointer passed to remove must be EXACTLY the pointer that was passed to
+// Add.  It is not sufficient for it to be a different pointer to the same
+// function!  This is because there is no function equality in Go.
 func (c *CallbackChain) Remove(p *func(l *LLAPPacket)) {
 	c.l.Lock()
 	defer c.l.Unlock()
@@ -32,6 +40,7 @@ func (c *CallbackChain) Remove(p *func(l *LLAPPacket)) {
 	delete(c.m, p)
 }
 
+// Run runs every callback in the chain with the given packet.
 func (c *CallbackChain) Run(p *LLAPPacket) {
 	c.l.RLock()
 	defer c.l.RUnlock()
