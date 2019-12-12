498a5c2683cb9b7aae64e022f2becd1e756259d3
Rob Mitchelmore <cheesey@prydwen.local>
2019-12-11 22:39:10 -0800

    More godoc

--- lib/localtalk/llap.go
+++ lib/localtalk/llap.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cheesestraws/appletalk/lib/ddp"
 )
 
+// An LLAPPacket contains information from a decoded LLAP packet.
 type LLAPPacket struct {
 	Src      uint8
 	Dst      uint8
@@ -14,6 +15,8 @@ type LLAPPacket struct {
 	Data     []byte
 }
 
+// EncodeBytes returns an LLAP packet as a slice of bytes ready to send to the
+// physical layer.
 func (l LLAPPacket) EncodeBytes() []byte {
 	buf := make([]byte, 3+len(l.Data), 3+len(l.Data))
 	buf[0] = l.Dst
@@ -27,6 +30,8 @@ var IncompleteHeader error = errors.New("incomplete LLAP header")
 var Short error = errors.New("packet data too short for its declared LLAP length")
 var NotDDP error = errors.New("not a DDP packet")
 
+// DecodeLLAPPacket takes an LLAP packet as a slice of bytes read from the 
+// listener and parses it into an LLAPPacket struct.
 func DecodeLLAPPacket(p []byte) (*LLAPPacket, error) {
 	if len(p) < 3 {
 		return nil, IncompleteHeader
@@ -43,11 +48,14 @@ func DecodeLLAPPacket(p []byte) (*LLAPPacket, error) {
 	return &packet, nil
 }
 
+// PrettyHeaders returns a summary of the LLAP header of the packet suitable
+// for human consumption
 func (l *LLAPPacket) PrettyHeaders() string {
 	return fmt.Sprintf("from %s to %s, type %s, %d data bytes",
 		llapAddressString(l.Dst), llapAddressString(l.Src), llapTypeString(l.LLAPType), len(l.Data))
 }
 
+// DDP isn't finished.
 func (l *LLAPPacket) DDP() (*ddp.Packet, error) {
 	if l.LLAPType == 1 {
 		return ddp.DecodePacket(l.Data, true, false)
--- lib/localtalk/llap_packet_types.go
+++ lib/localtalk/llap_packet_types.go
@@ -1,7 +1,7 @@
 package localtalk
 
 const LAPLowestControlPacketType = 0x80
-const LAPENQ = 0x81
-const LAPACK = 0x82
-const LAPRTS = 0x84
-const LAPCTS = 0x85
+const LAPENQ = 0x81 // The LLAPType for an LLAP ENQ packet
+const LAPACK = 0x82 // The LLAPType for an LLAP ACK packet
+const LAPRTS = 0x84 // The LLAPType for an LLAP RTS packet
+const LAPCTS = 0x85 // The LLAPType for an LLAP CTS packet
